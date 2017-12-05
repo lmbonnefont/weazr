7ad529163aad2415c2731fa30d45b8b47dbde272
charles edange <charlesedange@hotmail.com>
2017-12-05 12:00:08 +0100

    devise, ajout routes et creation des vues contact about

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
 end
