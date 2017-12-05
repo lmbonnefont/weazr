6d7150dc0d89bf0da6c0e172a9a05ef4f1824b86
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-12-05 14:47:27 +0100

    ajout des controllers pour créer une compagny

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
-  resources :companys
+  resources :companies do
+    resources :inputs, only: [:new, :create, :update, :edit]
+  end
 end
