6cbc0e1910afcfa255fba0db48bcf929534eba63
guillaume lestrade <guillaumelestrade@gmail.com>
2017-12-06 12:07:47 +0100

    mise en place de la page html et css du dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:home, :contact]
+
+  def after_sign_up_path_for(resource)
+      '/an/example/path' # Or :prefix_to_your_route
+  end
+
 end
