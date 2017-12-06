f23f6d4e649931071c09cfe4546b0fefcf2a0808
Aurelien Allard <allard.au@gmail.com>
2017-12-06 17:52:35 +0100

    Ad creative done

--- app/fb_ads/api_calls.rb
+++ app/fb_ads/api_calls.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 require 'facebook_ads'
 
 class FacebookAdsAPIClient
@@ -6,6 +7,7 @@ class FacebookAdsAPIClient
     FacebookAds.access_token = 'EAAVcDLP8LsoBAJsIacGWn93YSlf0xbrYqAT94xPdX96yZC59O3pjgVFQpD4NHYvaSXpAwu2ytjImY1XaFbuo5gwUgNk0bHQHAOxWmyUR8ZBsrqBCvpUYKfqd6FnbrWi8TBjIChRjqZAK1xFMmqLdYIL8z9llzRNhV8kHahgtAZDZD'
     FacebookAds.base_uri = 'https://graph.facebook.com/v2.11'
     @account = FacebookAds::AdAccount.find(account_id)
+    @account_id = account_id
     @campaign = FacebookAds::AdCampaign.find(campaign_id)
     @page_id = page_id
   end
@@ -31,55 +33,53 @@ class FacebookAdsAPIClient
   def create_campaign
   end
 
-  # def create_adimages
-  #   @account.create_ad_images([
-  #     'https://d38eepresuu519.cloudfront.net/485674b133dc2f1d66d20c9d52c62bec/original.jpg'
-  #   ])
-  # end
+  def create_adimages(url)
+    FacebookAds.access_token = 'EAAVcDLP8LsoBAJsIacGWn93YSlf0xbrYqAT94xPdX96yZC59O3pjgVFQpD4NHYvaSXpAwu2ytjImY1XaFbuo5gwUgNk0bHQHAOxWmyUR8ZBsrqBCvpUYKfqd6FnbrWi8TBjIChRjqZAK1xFMmqLdYIL8z9llzRNhV8kHahgtAZDZD'
+    dl = download(url)
+    file = File.open(dl[1])
+    uri = "#{FacebookAds.base_uri}/#{@account_id}/adimages"
+    response = RestClient.post(uri, {dl[0] => file, objectivity: false, access_token: FacebookAds.access_token })
+  end
 
   def create_adcreative
+    ad_image_hash = account.ad_images.first["hash"]
+    image_ad_creative = account.create_ad_creative({
+      name: 'Test Single Image Creative',
+      page_id: @page_id, # Add your Facebook Page ID here.
+      message: 'A message.',
+      link: 'https://www.facebook.com/Kibouftou-1917026111950285/', # Add your Play Store ID here.
+      link_title: 'A link title.',
+      image_hash: ad_image_hash,
+      call_to_action_type: 'SHOP_NOW'
+    }, creative_type: 'image')
   end
 
   def create_adset
   end
+
+  private
+
+  def download(url)
+    pathname = Pathname.new(url)
+    name = "#{pathname.dirname.basename}.jpg"
+    data = RestClient.get(url).body
+    file = File.open("/tmp/#{name}", 'w')
+    file.binmode
+    file.write(data)
+    file.close
+    [name, file.path]
+  end
+
 end
 
-require 'pathname'
-def download(url)
-  pathname = Pathname.new(url)
-  name = "#{pathname.dirname.basename}.jpg"
-  # @FIXME: Need to handle exception here.
-  data = RestClient.get(url).body
-  file = File.open("/tmp/#{name}", 'w') # Assume *nix-based system.
-  file.binmode
-  file.write(data)
-  file.close
-  [name, file.path]
-end
 
 
 this = FacebookAdsAPIClient.new('act_114566172663449', '23842663923640452', '1917026111950285')
 account = this.get_account
 campaign = this.get_campaign
-FacebookAds.access_token = 'EAAVcDLP8LsoBAJsIacGWn93YSlf0xbrYqAT94xPdX96yZC59O3pjgVFQpD4NHYvaSXpAwu2ytjImY1XaFbuo5gwUgNk0bHQHAOxWmyUR8ZBsrqBCvpUYKfqd6FnbrWi8TBjIChRjqZAK1xFMmqLdYIL8z9llzRNhV8kHahgtAZDZD'
-url = 'https://d38eepresuu519.cloudfront.net/485674b133dc2f1d66d20c9d52c62bec/original.jpg'
-dl = download(url)
-file = File.open(dl[1])
-# response = AdImage.post("/act_114566172663449/adimages", query: {"original" => url })
-# query = pack(query, objectify: false)
-uri = "#{FacebookAds.base_uri}/act_114566172663449/adimages"
-p response = RestClient.post(uri, {dl[0] => file, objectivity: false, access_token: FacebookAds.access_token })
-# account.create_ad_images(['https://d38eepresuu519.cloudfront.net/485674b133dc2f1d66d20c9d52c62bec/original.jpg'])
-
-# account.ad_images
-
-# puts "1. #{account.name}"
-# puts "2. #{campaign.name}"
-# puts
+# this.create_adimages('http://www.seriouseats.com/recipes/assets_c/2016/04/20160418-american-omelet-ham-and-cheese-21-thumb-1500xauto-431186.jpg')
 
 
-# urls = ['https://d38eepresuu519.cloudfront.net/485674b133dc2f1d66d20c9d52c62bec/original.jpg', 'https://d38eepresuu519.cloudfront.net/3977d2a47b584820969e2acf4d923e33/original.jpg']
-# p files = urls.map do |url|
-#   name, path = download(url)
-#   [name, File.open(path)]
-# end.to_h
+
+
+puts "-------- Done --------"
