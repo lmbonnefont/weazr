695d56a631f3296ee5a1862d63091ed740009277
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-12-06 11:22:53 +0100

    creation campagne tt inclu

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,45 @@
+class CampaignsController < ApplicationController
+  before_action :set_campaign, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    @campaign.company = current_user.companies.first
+    @campaign.budget_remaining = @campaign.budget_total
+    @campaign.save
+    redirect_to campaign_path(@campaign)
+  end
+
+  def edit
+  end
+
+  def update
+    @campaign.update(campaign_params)
+    @campaign.save
+    redirect_to campaign_path(@campaign)
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+    @campaign.destroy
+    redirect_to profile_path(current_user)
+  end
+
+  private
+
+  def set_campaign
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def campaign_params
+    params.require(:campaign).permit(:start, :end, :budget_total, :title)
+  end
+end
---
+++ db/migrate/20171206094156_create_campaigns.rb
@@ -0,0 +1,14 @@
+class CreateCampaigns < ActiveRecord::Migration[5.1]
+  def change
+    create_table :campaigns do |t|
+      t.references :company, foreign_key: true
+      t.date :start
+      t.date :end
+      t.integer :budget_total
+      t.integer :budget_remaining
+      t.boolean :live
+
+      t.timestamps
+    end
+  end
+end
