ac9ec678da9deb3996eeef2715c4d29993d8dd0a
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-12-12 18:06:35 +0100

    remove pb migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Weather.destroy_all
 Meteo.destroy_all
 Dashboard.destroy_all
 Input.destroy_all
