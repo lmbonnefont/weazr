3a4ce3199271296aa50064aef2f80d7f1b01e8d5
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-12-12 11:42:38 +0100

    fixing bug and migrate

--- db/migrate/20171211183935_remove_indices_from_campaign_days.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIndicesFromCampaignDays < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :campaign_days, :irain, :float
-    remove_column :campaign_days, :idamp, :float
-    remove_column :campaign_days, :itemperature, :float
-    remove_column :campaign_days, :idailyabsolute, :float
-    remove_column :campaign_days, :idailyrelative, :float
-  end
-end
