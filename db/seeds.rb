50838fee54838eb52a45042d492e5f1858f610e3
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-12-12 18:12:53 +0100

    fixing heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Weather.destroy_all
 Meteo.destroy_all
 Dashboard.destroy_all
 Input.destroy_all
