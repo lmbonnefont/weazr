956833150ddee75bb6722c2d05284cd2c63b5a0d
guillaume lestrade <guillaumelestrade@gmail.com>
2017-12-13 11:50:06 +0100

    rien

--- db/migrate/20171213091732_add_title_to_campaigns.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToCampaigns < ActiveRecord::Migration[5.1]
-  def change
-    add_column :campaigns, :title, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20171213091732) do
-
+ActiveRecord::Schema.define(version: 20171212164502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +50,6 @@ ActiveRecord::Schema.define(version: 20171213091732) do
     t.string "url"
     t.boolean "display", default: true
     t.float "usualbudget"
-
-    t.string "title"
     t.string "cpm"
     t.string "cpc"
     t.string "post_engagements"
