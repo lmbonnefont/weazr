e8f91ce1d911c4af93503ee20c0f1a6ee6296c96
guillaume lestrade <guillaumelestrade@gmail.com>
2017-12-14 18:16:28 +0100

    front page dash

--- app/javascript/components/fb-show.js
+++ app/javascript/components/fb-show.js
@@ -1,7 +1,7 @@
 function one() {
   const title = document.getElementById('campaign_title');
   title.addEventListener("blur", (event) => {
-    const title_val = document.getElementById('campaign_title').value;
+    var title_val = document.getElementById('campaign_title').value;
     document.getElementById('fb-show-title').innerHTML = title_val;
   });
 
@@ -9,14 +9,14 @@ function one() {
 
   const post_title = document.getElementById('campaign_post_title');
   post_title.addEventListener("blur", (event) => {
-    const post_title_val = document.getElementById('campaign_post_title').value;
+    var post_title_val = document.getElementById('campaign_post_title').value;
     document.getElementById('title-pic-fb').innerHTML = post_title_val;
   });
 
 
   const post_msg = document.getElementById('campaign_post_msg');
   post_msg.addEventListener("blur", (event) => {
-    const post_msg_val = document.getElementById('campaign_post_msg').value;
+    var post_msg_val = document.getElementById('campaign_post_msg').value;
     document.getElementById('content-pic-fb').innerHTML = post_msg_val;
   });
 
