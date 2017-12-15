29b18220d4d9c30d65759953636559ace79b85a6
charles edange <charlesedange@hotmail.com>
2017-12-15 16:42:18 +0100

    fix js

--- app/javascript/components/fb-show.js
+++ app/javascript/components/fb-show.js
@@ -1,24 +1,29 @@
 function one() {
   const title = document.getElementById('campaign_title');
-  title.addEventListener("blur", (event) => {
-    var title_val = document.getElementById('campaign_title').value;
-    document.getElementById('fb-show-title').innerHTML = title_val;
-  });
-
+  if (title != null) {
+    title.addEventListener("blur", (event) => {
+      var title_val = document.getElementById('campaign_title').value;
+      document.getElementById('fb-show-title').innerHTML = title_val;
+    });
+  }
 
 
   const post_title = document.getElementById('campaign_post_title');
-  post_title.addEventListener("blur", (event) => {
-    var post_title_val = document.getElementById('campaign_post_title').value;
-    document.getElementById('title-pic-fb').innerHTML = post_title_val;
-  });
+  if (post_title != null) {
+    post_title.addEventListener("blur", (event) => {
+      var post_title_val = document.getElementById('campaign_post_title').value;
+      document.getElementById('title-pic-fb').innerHTML = post_title_val;
+    });
+  }
 
 
   const post_msg = document.getElementById('campaign_post_msg');
-  post_msg.addEventListener("blur", (event) => {
-    var post_msg_val = document.getElementById('campaign_post_msg').value;
-    document.getElementById('content-pic-fb').innerHTML = post_msg_val;
-  });
+  if (post_msg != null) {
+    post_msg.addEventListener("blur", (event) => {
+      var post_msg_val = document.getElementById('campaign_post_msg').value;
+      document.getElementById('content-pic-fb').innerHTML = post_msg_val;
+    });
+  }
 
 
 }
